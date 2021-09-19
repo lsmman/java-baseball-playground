9ee404abe98b5392d3c0bd19980b0773de4746d5
lsmman <lsmman07@gmail.com>
2021-09-19 20:44:34 +0900

    fix build.gradle in order to fit latest gradle version

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
