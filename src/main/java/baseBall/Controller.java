1cff6550ccb2cfb26fffb3cbeb2cd5002f0ecc0e
lsmman <lsmman07@gmail.com>
2021-10-25 17:18:01 +0900

    feat(baseBall): add Controller by managing UI, Service

--- src/main/java/baseBall/Controller.java
+++ src/main/java/baseBall/Controller.java
@@ -1,4 +1,36 @@
 package baseBall;
 
+import baseBall.Dto.GameResponseDto;
+
 public class Controller {
+    Service service;
+    UI ui;
+
+    public Controller(UI ui) {
+        this.ui = ui;
+        service = new Service();
+    }
+
+    public void play() {
+        boolean reGame = true;
+        while (reGame) {
+            playGame();
+            reGame = ui.reGameQuestionView();
+        }
+    }
+
+    private void playGame() {
+        service.initGame();
+        String inputStr;
+
+        while (true) {
+            inputStr = ui.inputView();
+            GameResponseDto gameResponseDto = service.playGame(inputStr);
+            ui.resultView(gameResponseDto);
+            if (gameResponseDto.isSuccess()) {
+                ui.gameEndView();
+                break;
+            }
+        }
+    }
 }
