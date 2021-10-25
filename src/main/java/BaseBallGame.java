f509e833e613b3343dc38f43722e0122704ec7cf
lsmman <lsmman07@gmail.com>
2021-10-25 17:18:41 +0900

    feat(baseBall): add BaseBallGame as main

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BaseBallGame {
+import baseBall.Controller;
+import baseBall.UI;
+
+public class BaseBallGame {
+    UI ui;
+    Controller controller;
+
+    public BaseBallGame(){
+        ui = new UI();
+        controller = new Controller(ui);
+    }
+
+    private void gameStart() {
+        controller.play();
+    }
+
+    public static void main(String[] args) {
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.gameStart();
+    }
+
 }
